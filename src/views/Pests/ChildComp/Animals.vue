fa1db1547810e4ffbfcf0c23ec94e9d32c065e56
syhua4 <syhua4@student.monash.edu>
2020-09-05 03:59:05 +1000

    Create the Animal and Plants pages

---
+++ src/views/Pests/ChildComp/Animals.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="animals">
+    <modal :title="speciesInfo.COMMON_NAM" v-show="Object.keys(speciesInfo).length">
+      <tab slot="modal-content" :titles="tabTitles" :speciesInfo="speciesInfo" />
+    </modal>
+
+    <gallery :content="animals" @showSpecies="showSpecies" />
+  </div>
+</template>
+
+<script>
+import Gallery from 'components/content/Gallery';
+import Modal from 'components/content/Modal';
+import Tab from 'components/content/Tab';
+
+import { getAnimals } from 'networks/api';
+export default {
+  name: 'Animals',
+  components: { Gallery, Modal, Tab },
+  async created() {
+    this.loading = this.$loading({
+      lock: true,
+      text: 'Loading',
+      spinner: 'el-icon-loading',
+      background: 'rgba(0, 0, 0, 0.7)'
+    });
+    await getAnimals().then(res => {
+      this.animals = res.data;
+    });
+  },
+  destroyed() {
+    this.$parent.$refs.wrapper.style.position = '';
+  },
+  data() {
+    return {
+      loading: null,
+      animals: [],
+      tabTitles: ['Species Information', 'Impact to Bushland', 'Help the Bushland'],
+      speciesInfo: {}
+    };
+  },
+  methods: {
+    showSpecies(index) {
+      this.speciesInfo = this.animals[index];
+    }
+  },
+  watch: {
+    animals: {
+      deep: true,
+      handler(val) {
+        if (val.length) this.loading.close();
+      }
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.animals {
+  margin: 0 auto;
+  @include font_size($m);
+}
+</style>
---
+++ src/views/Pests/ChildComp/Plants.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="plants"></div>
+</template>
+
+<script>
+export default {
+  name: 'Plants'
+};
+</script>
+
+<style lang="scss" scoped>
+.plants {
+  @include font_size($m);
+}
+</style>
