8d4faa9a5118a066faa0e5e42b99412d09702eb6
syhua4 <syhua4@student.monash.edu>
2020-10-04 05:28:05 +1100

    Completed the weed info page with the fullpage slider effect (using swiper component)

---
+++ src/components/common/Slider/Slider.js
@@ -0,0 +1,4 @@
+import Swiper from './Slider.vue';
+import SwiperItem from './SliderItem.vue';
+
+export { Swiper, SwiperItem };
---
+++ src/components/common/Slider/Slider.vue
@@ -0,0 +1,72 @@
+<template>
+  <swiper ref="mySwiper" :options="swiperOptions">
+    <slot />
+    <div class="swiper-pagination" slot="pagination"></div>
+  </swiper>
+</template>
+
+<script>
+import { Swiper } from 'vue-awesome-swiper';
+import 'swiper/css/swiper.css';
+
+export default {
+  name: 'Slider',
+  components: {
+    Swiper
+  },
+  props: {
+    options: {
+      type: Object,
+      default: () => {}
+    }
+  },
+  data() {
+    return {
+      swiperOptions: Object.assign(
+        {},
+        {
+          direction: 'vertical',
+          slidesPerView: 1,
+          mousewheel: true,
+          height: window.innerHeight,
+          keyboard: {
+            enabled: true
+          },
+          pagination: {
+            el: '.swiper-pagination',
+            clickable: true
+          }
+        },
+        this.options
+      )
+    };
+  },
+  methods: {
+    stopSwiper() {
+      this.$refs.mySwiper.$swiper.autoplay.stop();
+    },
+    startSwiper() {
+      this.$refs.mySwiper.$swiper.autoplay.start();
+    }
+  },
+  computed: {
+    swiper() {
+      return this.$refs.mySwiper.$swiper;
+    }
+  }
+};
+</script>
+<style lang="scss">
+.swiper-container {
+  height: 100%;
+  width: 100%;
+  .swiper-button-prev,
+  .swiper-button-next {
+    color: $sub-color;
+    transition: opacity 0.2s linear;
+  }
+  .swiper-pagination-bullet-active {
+    background: $background-color;
+  }
+}
+</style>
